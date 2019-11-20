d24839818c9dcfa555f2fc3bb9bb7210e33eb666
Drogovik <44835924+Drogovik@users.noreply.github.com>
2019-11-20 13:09:58 +0200

    Add files via upload

---
+++ test_spec.js
@@ -0,0 +1,12 @@
+describe("User Registration Page Tests", () => {
+    it("user registration success", () => {
+	browser.get("https://github.com/");
+	// element(by.model('user[login]')).sendKeys('some')
+	    element(by.model('user[login]')).sendKeys('some');
+        element(by.model('user[email]')).sendKeys('some'+'@user.com');
+        element(by.model('user[password]')).sendKeys('123456');
+        element(by.id('submitBtn')).click();
+		browser.getCurrentUrl().then(function(url) {
+            expect(url).toEqual('https://github.com/');     
+        });
+	});
