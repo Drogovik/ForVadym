5fee24e1bf90ed49b415d4ff894b8645e97c9534
Drogovik <44835924+Drogovik@users.noreply.github.com>
2019-11-21 14:13:01 +0200

    Add files via upload

--- test_spec.js
+++ test_spec.js
@@ -1,13 +1,28 @@
+  
 describe("User Registration Page Tests", () => {
-    it("user registration success", () => {
-	browser.get("https://github.com/");
-	// element(by.model('user[login]')).sendKeys('some')
+    it("user registration success", () => {    
 	    element(by.model('user[login]')).sendKeys('some');
         element(by.model('user[email]')).sendKeys('some'+'@user.com');
         element(by.model('user[password]')).sendKeys('123456');
         element(by.id('submitBtn')).click();
-		browser.getCurrentUrl().then(function(url) {
-            expect(url).toEqual('https://github.com/');     
+		browser.ignoreSynchronization = true;
+		browser.getCurrentUrl().then(function(url)
+	    browser.waitForAngularEnabled(false);	
+	{
+            expect(url).toEqual('https://github.com/'); 
+		
         });
 	});
 	});
+
+	
+	
+// browser.waitForAngularEnabled(false);
+// browser.get('/non-angular-login-page.html');
+
+// element(by.id('username')).sendKeys('Jane');
+// element(by.id('password')).sendKeys('1234');
+// element(by.id('clickme')).click();
+
+// browser.waitForAngularEnabled(true);
+// browser.get('/page-containing-angular.html');
